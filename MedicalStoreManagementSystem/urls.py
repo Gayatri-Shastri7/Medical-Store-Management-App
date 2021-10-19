af10532ef0f17a419747f6d0b11daf4908a91e4d
Gayatri <gayatrishastri777@gmail.com>
2021-10-19 11:35:46 +0530

    Second Commit

--- MedicalStoreManagementSystem/urls.py
+++ MedicalStoreManagementSystem/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from DjangoMedicalApp import views
+
+router=routers.DefaultRouter()
+router.register("company",views.CompanyViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(router.urls))
 ]
