2a693c04c076ac92dfb7fd95c29ed0986469d7e1
Gayatri <gayatrishastri777@gmail.com>
2021-10-19 13:10:44 +0530

    Third Commit

--- DjangoMedicalApp/views.py
+++ DjangoMedicalApp/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from DjangoMedicalApp.models import Company, CompanyBank, Medicine, MedicalDetails, CompanyAccount, Employee, \
+    EmployeeBank, EmployeeSalary, CustomerRequest, Bill, BillDetails
+    
+from DjangoMedicalApp.serializers import CompanySerliazer, CompanyBankSerializer, MedicineSerliazer, \
+    MedicalDetailsSerializer, MedicalDetailsSerializerSimple, CompanyAccountSerializer, EmployeeSerializer, \
+    EmployeeBankSerializer, EmployeeSalarySerializer, CustomerSerializer, BillSerializer, BillDetailsSerializer, \
+    CustomerRequestSerializer
+
+
+class CompanyViewSet(viewsets.ViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def list(self,request):
+        company=Company.objects.all()
+        serializer=CompanySerliazer(company,many=True,context={"request":request})
+        response_dict={"error":False,"message":"All Company List Data","data":serializer.data}
+        return Response(response_dict)
+
+    def create(self,request):
+        try:
+            serializer=CompanySerliazer(data=request.data,context={"request":request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            dict_response={"error":False,"message":"Company Data Save Successfully"}
+        except:
+            dict_response={"error":True,"message":"Error During Saving Company Data"}
+        return Response(dict_response)
+
+    def retrieve(self, request, pk=None):
+        queryset = Company.objects.all()
+        company = get_object_or_404(queryset, pk=pk)
+        serializer = CompanySerliazer(company, context={"request": request})
+
+        serializer_data = serializer.data
+        # Accessing All the Medicine Details of Current Medicine ID
+        company_bank_details = CompanyBank.objects.filter(company_id=serializer_data["id"])
+        companybank_details_serializers = CompanyBankSerializer(company_bank_details, many=True)
+        serializer_data["company_bank"] = companybank_details_serializers.data
+
+        return Response({"error": False, "message": "Single Data Fetch", "data": serializer_data})
+
+    def update(self,request,pk=None):
+        try:
+            queryset=Company.objects.all()
+            company=get_object_or_404(queryset,pk=pk)
+            serializer=CompanySerliazer(company,data=request.data,context={"request":request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            dict_response={"error":False,"message":"Successfully Updated Company Data"}
+        except:
+            dict_response={"error":True,"message":"Error During Updating Company Data"}
+
+        return Response(dict_response)
+    
+company_list=CompanyViewSet.as_view({"get":"list"})
+company_creat=CompanyViewSet.as_view({"post":"create"})
+company_update=CompanyViewSet.as_view({"put":"update"})
--- MedicalStoreManagementSystem/urls.py
+++ MedicalStoreManagementSystem/urls.py
@@ -13,15 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from DjangoMedicalApp import views
+from MedicalStoreManagementSystem import settings
 
 router=routers.DefaultRouter()
-router.register("company",views.CompanyViewSet)
+router.register("company",views.CompanyViewSet,basename="company")
+
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls))
+    path('api/',include(router.urls)),
+    path('api/gettoken/',TokenObtainPairView.as_view(),name="gettoken"),
+    path('api/resfresh_token/',TokenRefreshView.as_view(),name="refresh_token"),
 ]
